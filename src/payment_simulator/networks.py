2dc1457cce1aece0e43255192112ae62f96ce80d
Hanzholah Shobri <62739339+hanzholahs@users.noreply.github.com>
2024-08-31 11:49:11 +0700

    Update docstring

--- src/payment_simulator/networks.py
+++ src/payment_simulator/networks.py
@@ -6,9 +6,40 @@ from numpy.random import randint
 
 
 class AbstractPaymentNetwork(ABC):
+    """
+    An abstract base class for creating payment networks that simulate transactions
+    between banks within a financial system.
+
+    Attributes
+    ----------
+    total_banks : int
+        The total number of banks included in the network simulation.
+    G : networkx.Graph
+        The graph representing the payment network, where nodes represent banks
+        and edges represent transactions between them.
+
+    Methods
+    -------
+    simulate_payments(init_banks: int | None)
+        Simulates transactions across the network. This method must be implemented by subclasses.
+    extract_link_matrix(prop: bool = True) -> np.ndarray
+        Returns the adjacency matrix of the network, either as raw counts or as proportions.
+    _create_transaction()
+        Internal method to create a random transaction between banks based on predefined probabilities.
+    _random_bank(prob: np.ndarray) -> int
+        Selects a bank for initiating a transaction based on a weighted probability distribution.
+    _payment_link(sender: int, receiver: int)
+        Establishes or updates a transaction link between two banks.
+    """
+
     def __init__(self, total_banks: int) -> None:
         """
-        Initializes the abstract payment network with a graph attribute.
+        Initializes the payment network with the specified number of banks.
+
+        Parameters
+        ----------
+        total_banks : int
+            Specifies the total number of banks to include in the network.
         """
         self.G: nx.Graph = None
         self.total_banks = total_banks
@@ -16,24 +47,30 @@ class AbstractPaymentNetwork(ABC):
     @abstractmethod
     def simulate_payments(self, init_banks: int | None):
         """
-        Abstract method to simulate payments.
-        This method should be implemented by subclasses.
+        Abstract method to simulate payments between banks in the network.
+        Must be implemented by all subclasses to define specific simulation strategies.
+
+        Parameters
+        ----------
+        init_banks : int, optional
+            The number of banks that start transacting at the initiation of the simulation.
         """
         pass
 
     def extract_link_matrix(self, prop: bool = True) -> np.ndarray:
         """
-        Extracts and returns the adjacency matrix of the network graph.
+        Retrieves the adjacency matrix of the network, showing transaction links between banks.
 
         Parameters
         ----------
         prop : bool, optional
-            If True, returns the matrix as proportions of the total transactions. If False, returns raw transaction counts.
+            Determines whether the matrix should show proportions of total transactions (True)
+            or raw transaction counts (False). Defaults to True.
 
         Returns
         -------
         np.ndarray
-            A numpy ndarray representing the adjacency matrix of the network graph.
+            The adjacency matrix of the network.
         """
         matrix = nx.to_numpy_array(self.G, weight="weight")
         if not prop:
@@ -42,7 +79,8 @@ class AbstractPaymentNetwork(ABC):
 
     def _create_transaction(self):
         """
-        Creates a transaction between randomly selected banks in the network.
+        Internally generates a random transaction by selecting a sender and receiver from the network.
+        The selection is influenced by the preferential attachment vector 'h'.
         """
         # select sender and receiver
         prob = self.h / self.h.sum()
@@ -62,23 +100,23 @@ class AbstractPaymentNetwork(ABC):
 
     def _random_bank(self, prob) -> int:
         """
-        Selects a bank for initiating a transaction based on a weighted probability distribution.
+        Randomly selects a bank to initiate a transaction, using a weighted probability distribution.
 
         Parameters
         ----------
         prob : np.ndarray
-            Array of probabilities for each bank.
+            An array of probabilities for each bank, indicating the likelihood of each bank initiating a transaction.
 
         Returns
         -------
         int
-            The selected bank's identifier.
+            The identifier of the bank selected to initiate the transaction.
         """
         return np.random.choice(self.G.nodes(), p=prob)
 
     def _payment_link(self, sender: int, receiver: int) -> None:
         """
-        Creates or updates a payment link between two banks in the simulation graph.
+        Creates or updates a payment link between two banks.
 
         Parameters
         ----------
@@ -86,6 +124,11 @@ class AbstractPaymentNetwork(ABC):
             The identifier of the bank initiating the payment.
         receiver : int
             The identifier of the bank receiving the payment.
+
+        Notes
+        -----
+        This method increments the weight of the edge between the sender and receiver to reflect
+        the occurrence of a transaction.
         """
         if self.G.has_edge(sender, receiver):
             self.G[sender][receiver]["weight"] += 1
@@ -102,18 +145,21 @@ class SimplePaymentNetwork(AbstractPaymentNetwork):
         allow_self_loop: bool = False,
     ) -> None:
         """
-        Initializes the RTGS Simulator with specified parameters.
+        Initializes a simple payment network simulation, defining parameters such as the number of banks,
+        the average number of transactions per bank, the strength of preferential attachment, and whether
+        self-transactions (self-loops) are permitted.
 
         Parameters
         ----------
         total_banks : int
-            Total number of banks participating in the RTGS simulation.
+            The total number of banks participating in the simulation.
         avg_payments : int
-            Average number of payments each bank is expected to process during the simulation.
+            The average number of transactions that each bank is expected to process.
         alpha : float, optional
-            A learning rate parameter that influences the strength of preferential attachment in the simulation.
+            The learning rate parameter that influences the strength of preferential attachment in the network,
+            default is 0, which implies no preferential attachment is considered.
         allow_self_loop : bool, optional
-            Boolean indicating whether transactions within the same bank (self-loops) are allowed.
+            Indicates whether transactions within the same bank are allowed, default is False.
         """
         super().__init__(total_banks=total_banks)
 
@@ -126,14 +172,27 @@ class SimplePaymentNetwork(AbstractPaymentNetwork):
         self, init_banks: int | None = None, increment: int = 2
     ) -> None:
         """
-        Simulates the payment processing between banks for a given period, starting with an initial set of banks.
+        Simulates the payment processing between banks, gradually increasing the number of participating banks
+        according to specified parameters. The simulation starts with an initial set of banks and incrementally adds
+        more until all banks are active.
 
         Parameters
         ----------
         init_banks : int, optional
-            Initial number of banks to start the simulation with. If None, it defaults to half of the total banks rounded up.
+            The initial number of banks to include in the simulation. If not specified, it defaults to half of the total banks, rounded up.
         increment : int, optional
-            The number of banks to add in each iteration.
+            The number of additional banks added in each step of the simulation. Must be a positive integer.
+
+        Raises
+        ------
+        AssertionError
+            If `increment` is not a positive integer.
+
+        Notes
+        -----
+        The simulation dynamically adjusts the number of transactions based on the changing number of banks,
+        maintaining the average payments per bank. This process continues until all specified banks are included
+        in the network or the addition results in no new banks due to constraints.
         """
         assert increment > 0, "`increment` must be positive integer."
 
